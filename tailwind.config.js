d3ebf2e597ab1e830f0721f809342832d29f258f
dafahfabiansyah <davarobiansyah@gmail.com>
2023-05-23 22:37:57 +0700

    portfolio tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#a5b4fc',
+        secondary: '#64748b',
+        dark: '#020617',
+      },
+    },
+  },
+  plugins: [],
+};
