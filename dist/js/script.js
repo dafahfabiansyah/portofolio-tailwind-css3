953933fc3ef9e44e538fc28534a225beb6e52335
dafahfabiansyah <davarobiansyah@gmail.com>
2023-05-24 09:14:33 +0700

    update fitur contact form

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,23 @@ hamburger.addEventListener('click', function () {
   hamburger.classList.toggle('hamburger-active');
   navMenu.classList.toggle('hidden');
 });
+
+// Contact form
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx6cdi22VGvDoMO-50m2CUSziI5eUCvreVJKTZTs10b9U2t3_eeLAZMuQMM8uEBxctC/exec';
+const form = document.forms['dafbians-contact-form'];
+const btnSender = document.querySelector('.btn-send');
+const myAlert = document.querySelector('.my-alert');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      myAlert.classList.toggle('hidden');
+      myAlert.addEventListener('click', function () {
+        document.querySelector('.my-alert').style.display = 'none';
+      });
+      form.reset();
+      console.log('Success!', response);
+    })
+    .catch((error) => console.error('Error!', error.message));
+});
